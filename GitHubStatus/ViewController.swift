b0661dc7f5782b592be6aab8cf0fc236bcf7f5e2
Tprezioso <tommyprezioso@gmail.com>
2017-02-16 12:25:00 -0500

    setup views for JSON objects

--- GitHubStatus/ViewController.swift
+++ GitHubStatus/ViewController.swift
@@ -11,6 +11,8 @@ import Alamofire
 
 class ViewController: UIViewController {
     @IBOutlet var statusLabel: UILabel!
+    @IBOutlet var lastUpdatedLabel: UILabel!
+    @IBOutlet var bodyLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,10 +20,19 @@ class ViewController: UIViewController {
             if let JSON = response.result.value {
                 print("JSON: \(JSON)")
                 let dataFromJson = JSON as? [String: Any]
-                print("\(dataFromJson?["status"]!)")
-                self.statusLabel.text = dataFromJson?["status"] as! String?
+               // print("\(dataFromJson?["created_on"]!)")
+                let statusString = dataFromJson?["status"] as! String?
+                self.statusLabel.text = "GitHub Status:\n\(statusString!)"
+                self.bodyLabel.text = dataFromJson?["body"] as! String?
+                self.lastUpdatedLabel.text = dataFromJson?["created_on"] as! String?
             }
         }
+        let image : UIImage = UIImage(named: "Octicons-mark-github.svg.png")!
+        let imageView1 = UIImageView(frame: CGRect(x:0, y: 0, width: 40, height: 40))
+        imageView1.contentMode = .scaleAspectFit
+        imageView1.image = image
+        self.navigationItem.titleView = imageView1
+        self.view.backgroundColor = UIColor.green
     }
 }
 
