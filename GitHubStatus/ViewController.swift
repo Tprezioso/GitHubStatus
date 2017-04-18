20b35817448424965b201d495f869c60b7efe210
Tprezioso <tommyprezioso@gmail.com>
2017-04-17 20:04:47 -0400

    adding to comments for future tom

--- GitHubStatus/ViewController.swift
+++ GitHubStatus/ViewController.swift
@@ -105,7 +105,7 @@ class ViewController: UIViewController {
             hud.hideSuccess()
             let alert = UIAlertController(title: "No Network Connection", message: "It seem you have lost your internet connection", preferredStyle: .actionSheet)
             alert.addAction(UIAlertAction(title: "Reload", style: .default) { action in
-                // Reloads App 
+                // Reloads App On Alert button press 
                 self.startTheApp()
             })
             self.present(alert, animated: true)
