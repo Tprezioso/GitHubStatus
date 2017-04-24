c6b0e177ad9eb8b90843dcf56f04d779c59a1707
Tprezioso <tommyprezioso@gmail.com>
2017-04-24 17:05:42 -0400

    working on launch screen

---
+++ GitHubStatus/LaunchScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenViewController.swift
+//  GitHubStatus
+//
+//  Created by Thomas Prezioso on 4/24/17.
+//  Copyright © 2017 Thomas Prezioso. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
