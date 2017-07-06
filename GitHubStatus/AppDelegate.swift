82386a0c85fee5737bc6213225b4c0e1f6991e0f
Tprezioso <tommyprezioso@gmail.com>
2017-07-06 16:49:43 -0400

    left a comment because notications isnt working

--- GitHubStatus/AppDelegate.swift
+++ GitHubStatus/AppDelegate.swift
@@ -86,6 +86,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
         if launchedBefore  {
             // TODO : issue with checking for user token stoping push notifications
+            // push notifications arent working when and when it isnt on
             print("Not first launch.")
         } else {
             print("First launch, setting UserDefault.")
